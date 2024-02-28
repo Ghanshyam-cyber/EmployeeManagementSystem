bde000523277f509ebe930c171a50aa87432325e
Ghanshyam Fulumbrikar <61972173+Ghanshyam-cyber@users.noreply.github.com>
2024-02-28 23:17:11 +0530

    Delete EmployeeManagementSysterm-Updated directory

--- EmployeeManagementSysterm-Updated/src/main/java/com/practice_project/EmployeeManagementSysterm/dao/EmployeeDao.java
+++
@@ -1,8 +0,0 @@
-package com.practice_project.EmployeeManagementSysterm.dao;
-
-import com.practice_project.EmployeeManagementSysterm.entity.Employee;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface EmployeeDao extends JpaRepository<Employee, Integer> {
-
-}
