b7b521bc95d088e7e0cba86b7103b41f1acb7db1
Ghanshyam Fulumbrikar <61972173+Ghanshyam-cyber@users.noreply.github.com>
2024-02-28 23:36:55 +0530

    Add files via upload

---
+++ EmployeeManagementSysterm-Deploying/src/main/java/com/practice_project/EmployeeManagementSysterm/entity/Employee.java
@@ -0,0 +1,87 @@
+package com.practice_project.EmployeeManagementSysterm.entity;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "employee")
+public class Employee {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private int id;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "age")
+    private int age;
+
+    @Column(name = "department")
+    private String department;
+
+    @Column(name = "email")
+    private String email;
+
+    public Employee(){
+
+    }
+
+    public Employee(String name, int age, String department, String email) {
+        this.name = name;
+        this.age = age;
+        this.department = department;
+        this.email = email;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getDepartment() {
+        return department;
+    }
+
+    public void setDepartment(String department) {
+        this.department = department;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    @Override
+    public String toString() {
+        return "Employee{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", age='" + age + '\'' +
+                ", department='" + department + '\'' +
+                ", email='" + email + '\'' +
+                '}';
+    }
+}
